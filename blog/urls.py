e19126370a4e64ab5f810a7d3b0b8902b7e66600
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-03-10 07:18:12 +0100

    blog list and blog detail functionality added

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
-
+# from . import views
+from .views import HomeView, BlogDetailView
 
 urlpatterns = [
-    path('', views.home, name='home')
+    # path('', views.home, name='home')
+    path('', HomeView.as_view(), name="blog"),
+    path('blogpost/<int:pk>', BlogDetailView.as_view(), name='blogpost'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+# def home(request):
+#     return render(request, 'home.html', {})
 
 
-def home(request):
-    return render(request, 'home.html', {})
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog_detail.html'
