fe8e9657a4bd41f1e5c297534270423f82a3553c
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-04-04 19:13:29 +0200

    Connected skills from database to homepage

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import SkillData, SkillWeb, SkillOther
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    skill_data = SkillData.objects.all()
+    skill_web = SkillWeb.objects.all()
+    skill_other = SkillOther.objects.all()
+
+    return render(request, 'home.html', {
+        'skill_data': skill_data,
+        'skill_web': skill_web,
+        'skill_other': skill_other
+    })
 
 
 def contact(request):
