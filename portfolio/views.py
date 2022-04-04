8c52b014282df08391ef3978105df496968958c0
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-04-04 19:28:47 +0200

    Connected projects from database to homepage

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,17 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import SkillData, SkillWeb, SkillOther
-
+from projects.models import Project
 
 def home(request):
     skill_data = SkillData.objects.all()
     skill_web = SkillWeb.objects.all()
     skill_other = SkillOther.objects.all()
 
+    projects = Project.objects.all().filter(frontpage=True)
+
     return render(request, 'home.html', {
         'skill_data': skill_data,
         'skill_web': skill_web,
-        'skill_other': skill_other
+        'skill_other': skill_other,
+        'projects': projects
     })
 
 
--- portfolioproject/urls.py
+++ portfolioproject/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls.static import static
 from django.urls import include, path
 from django.contrib import admin
+
+from portfolioproject import settings
 
 urlpatterns = [
     path('', include('portfolio.urls'), name="home"),
     path('blog', include('blog.urls'), name="blog"),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
