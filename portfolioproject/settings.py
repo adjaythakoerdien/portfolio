daac8cfa13b32c14b25cacf32c604c9de30a751c
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-04-04 19:37:36 +0200

    Fixed MEDIA URL + Added media url to urls.py

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -118,11 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'satic'
-# STATICFILES_DIRS = [
-#         BASE_DIR / 'static'
-# ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
