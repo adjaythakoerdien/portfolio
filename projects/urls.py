3a7a84035971e62c9394fd8e99e833b13b36b73d
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-04-07 07:05:51 +0200

    Project detail page pt 1

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.projects, name='projects'),
-    # path('contact', views.project, name='project')
-]
+    path('<int:project_id>/', views.project, name='project_detail'),
+    ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
 
 
@@ -7,3 +7,9 @@ def projects(request):
     projects_all = Project.objects.all()
 
     return render(request, 'projects_all.html', {"projects": projects_all})
+
+
+def project(request, project_id):
+    project_detail = get_object_or_404(Project, pk=project_id)
+
+    return render(request, 'project_detail.html', {'project': project_detail})
