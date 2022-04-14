f0139940bd479c2b5cd97bf2af256ea3144ee00b
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-04-14 16:01:17 +0200

    skill images fixes pt2

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,9 +4,9 @@ from .models import SkillData, SkillWeb, SkillOther
 from projects.models import Project
 
 def home(request):
-    skill_data = SkillData.objects.all()
-    skill_web = SkillWeb.objects.all()
-    skill_other = SkillOther.objects.all()
+    skill_data = SkillData.objects.all().order_by('number')
+    skill_web = SkillWeb.objects.all().order_by('number')
+    skill_other = SkillOther.objects.all().order_by('number')
 
     projects = Project.objects.all().filter(frontpage=True)
 
