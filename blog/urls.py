d60088da9154e15d7e59dcbde758f34bde1ebe45
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-04-14 13:48:19 +0200

    Project detail link and basic page added

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     # path('', views.home, name='home')
     path('', HomeView.as_view(), name="blog"),
-    path('blogpost/<int:pk>', BlogDetailView.as_view(), name='blogpost'),
+    path('/<int:pk>', BlogDetailView.as_view(), name='blogpost'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,12 @@
+from django.conf.urls.static import static
 from django.urls import path
+
+from portfolioproject import settings
 from . import views
 
 
 urlpatterns = [
     path('', views.projects, name='projects'),
-    path('<int:project_id>/', views.project, name='project_detail'),
-    ]
+    path('/<int:project_id>/', views.project, name='project_detail'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
